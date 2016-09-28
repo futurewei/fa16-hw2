4db350dfc99eb7f4c739ef7dde78ce8ad8f9631a
Lai Wei <98future@gmail.com>
2016-09-28 01:18:57 -0700

    finish

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,12 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  attr_accessor :string
+
   def initialize(param)
+  	 @s = param    
+  end
+
+  def bar(cat, sat:, dat:)
+  	return cat.to_s << @s << sat.to_s
   end
 end
--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -2,27 +2,36 @@ class Person
   attr_accessor :name, :age
 
   def initialize(name, age)
-    @name = name
+    @name = name[0,4]
     @age = age
   end
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'nickname'
+    @name
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    Time.now.year-@age.to_i
   end
 
   def introduction
-    # YOUR IMPLEMENTATION HERE
-    'introduce'
+   return @name, @age
+  end
+
+  def fibonacci(n)
+    if n==1
+      1
+    elsif n==2
+      1
+    else
+      fibonacci(n-1)+fibonacci(n-2)
+    end
   end
 
   def fib_number
     # YOUR IMPLMENTATION HERE
-    0
+    @var=fibonacci(@age.to_i)
   end
 end
